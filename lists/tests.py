f950cda5a20373b17f18be212bde3cdc70f33146
Manikanta Loya <mani.4272@gmail.com>
2024-07-13 23:12:06 -0700

    Refactored Home page view to use a template & some black formatting

--- lists/tests.py
+++ lists/tests.py
@@ -3,6 +3,7 @@ from typing_extensions import Self
 from django.urls import resolve
 from django.test import TestCase
 from django.http import HttpRequest
+from django.template.loader import render_to_string
 
 from lists.views import home_page
 
@@ -16,9 +17,14 @@ class HomePageTest(TestCase):
         self.assertEqual(found.func, home_page)
 
     def test_home_page_returns_correct_html(self: Self) -> None:
-        request = HttpRequest()
-        response = home_page(request)
+        response = self.client.get("/")
         html = response.content.decode("utf8")
         self.assertTrue(html.startswith("<html>"))
         self.assertIn("<title>To-Do Lists</title>", html)
-        self.assertTrue(html.endswith("</html>"))
+        self.assertTrue(html.strip().endswith("</html>"))
+
+        self.assertTemplateUsed(response, "home.html")
+
+    def test_uses_home_templates(self: Self) -> None:
+        response = self.client.get("/")
+        self.assertTemplateUsed(response, "home.html")
--- lists/views.py
+++ lists/views.py
@@ -5,4 +5,4 @@ import django.http as django_http
 
 
 def home_page(request: django_http.HttpRequest) -> django_http.HttpResponse:
-    return django_http.HttpResponse("<html><title>To-Do Lists</title></html>")
+    return render(request, "home.html")
